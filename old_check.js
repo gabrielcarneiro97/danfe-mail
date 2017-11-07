fc10abc93fdd3e460c1a21c0491ba53d7e1f9545
Gabriel Carneiro <gabriel.carneiro97@live.com>
2017-11-07 09:45:19 -0200

    Algumas notas não estavam sendo baixadas adequadamente, fiz uma pequena alteração que resolveu esse problema

--- old_check.js
+++ old_check.js
@@ -20,6 +20,43 @@ function findAttachmentParts(struct, attachments) {
   return attachments;
 }
 
+function buildAttMessageFunction(attachment, dir, num) {
+  var filename = attachment.params.name;
+  var encoding = attachment.encoding;
+
+  return function (msg, seqno) {
+
+  	if(!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+    var prefix = '(#' + seqno + ') ';
+    msg.on('body', function(stream, info) {
+      //Create a write stream so that we can stream the attachment to file;
+      console.log(prefix + 'Streaming this attachment to file', filename, info);
+      var writeStream = fs.createWriteStream(dir + "/" + filename);
+      writeStream.on('finish', function() {
+        console.log(prefix + 'Done writing to file %s', filename);
+
+        console.log(num)
+
+      });
+
+      //stream.pipe(writeStream); this would write base64 data to the file.
+      //so we decode during streaming using 
+      if (encoding === 'BASE64') {
+        //the stream is base64 encoded, so here the stream is decode on the fly and piped to the write stream (file)
+        stream.pipe(base64.decode()).pipe(writeStream);
+      } else  {
+        //here we have none or some other decoding streamed directly to the file which renders it useless probably
+        stream.pipe(writeStream);
+      }
+    });
+    msg.once('end', function() {
+      console.log(prefix + 'Finished attachment %s', filename);
+    });
+  };
+}
+
+
 var imap = new Imap({
   user: email,
   password: password,
@@ -28,12 +65,18 @@ var imap = new Imap({
   tls: true
 });
 
+var last = '';
+
+process.on('message', m => {
+	if(m.type === 'last'){
+		last = m.data;
+	}
+});
+
 imap.once('ready', function() {
   imap.openBox('Notas', true, function(err, box) {
-    let total = box.messages.total;
-    
+  	total = box.messages.total;
     if (err) throw err;
-
     var f = imap.seq.fetch(`${total - 10}:${total}` , {
       bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)'],
       struct: true
@@ -41,33 +84,30 @@ imap.once('ready', function() {
 
     f.on('message', function (msg, seqno) {
 
-      let prefix = '(#' + seqno + ') ';
+      var prefix = '(#' + seqno + ') ';
       let dir = './notas';
-      let attachments = [];
-      let num = undefined;
-      let name = undefined;
-      let attrs = undefined;
-
+      let num = 'undef';
       msg.on('body', function(stream, info) {
         var buffer = '';
         stream.on('data', function(chunk) {
           buffer += chunk.toString('utf8');
         });
-
         stream.once('end', function() {
-          name = Imap.parseHeader(buffer).subject[0].split(" - ")[2];
+          let name = Imap.parseHeader(buffer).subject[0].split(" - ")[2];
           num = Imap.parseHeader(buffer).subject[0].split(" - ")[1];
+          console.log(num)
           dir = fullDir + name;
+        	process.send({type:'seq', 
+      			data: {
+      				seq: seqno,
+      				name: name
+      			}
+      		});
         });
       });
+      msg.once('attributes', function(attrs) {
 
-      msg.once('attributes', function(a) {
-        attrs = a;
-        attachments = findAttachmentParts(attrs.struct);
-      });
-
-      msg.once('end', function() {
-
+        var attachments = findAttachmentParts(attrs.struct);
         for (var i = 0, len=attachments.length ; i < len; ++i) {
           var attachment = attachments[i];
           /*This is how each attachment looks like {
@@ -88,43 +128,11 @@ imap.once('ready', function() {
             bodies: [attachment.partID],
             struct: true
           });
-          let filename = attachment.params.name;
-          let encoding = attachment.encoding;
           //build function to process attachment message
-          f.on('message', function (msg, seqno) {
-
-            if(!fs.existsSync(dir)) fs.mkdirSync(dir);
-
-            var prefix = '(#' + seqno + ') ';
-
-            msg.on('body', function(stream, info) {
-              //Create a write stream so that we can stream the attachment to file;
-              console.log(prefix + 'Streaming this attachment to file', filename, info);
-              var writeStream = fs.createWriteStream(dir + "/" + filename);
-              writeStream.on('finish', function() {
-                console.log(prefix + 'Done writing to file %s', filename);
-
-                console.log(num)
-
-              });
-
-              //stream.pipe(writeStream); this would write base64 data to the file.
-              //so we decode during streaming using 
-              if (encoding === 'BASE64') {
-                //the stream is base64 encoded, so here the stream is decode on the fly and piped to the write stream (file)
-                stream.pipe(base64.decode()).pipe(writeStream);
-              } else  {
-                //here we have none or some other decoding streamed directly to the file which renders it useless probably
-                stream.pipe(writeStream);
-              }
-            });
-
-            msg.once('end', function() {
-              console.log(prefix + 'Finished attachment %s', filename);
-            });
-          });
-
+          f.on('message', buildAttMessageFunction(attachment, dir, num));
         }
+      });
+      msg.once('end', function() {
         console.log(prefix + 'Finished email');
       });
     });
