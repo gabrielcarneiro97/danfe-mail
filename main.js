8d429b5877ed8b252c4535c403277f8a9ac4ea8f
Gabriel Carneiro <gabriel.carneiro97@live.com>
2017-11-21 08:45:05 -0200

    Algumas linhas estavam indevidamente comentadas no arquivo main

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const cp = require('child_process'),
 	interval = consts.interval; //Define o intervalo como 60 segundos.
 	
 var invoke = () => {
-	//Inicializa o processo filho onde acontecerá o check da caixa direcionada.
-	// var check = cp.fork('./check.js', [], [ 'pipe', 'pipe', 'pipe', 'ipc' ]);
-	// check.on('message', m => {
-	// 	//Recebe mensagem para matar o processo filho.
-	// 	if(m.type === 'killme')
-	// 		check.kill('SIGINT')
-	// })
+	Inicializa o processo filho onde acontecerá o check da caixa direcionada.
+	var check = cp.fork('./check.js', [], [ 'pipe', 'pipe', 'pipe', 'ipc' ]);
+	check.on('message', m => {
+		//Recebe mensagem para matar o processo filho.
+		if(m.type === 'killme')
+			check.kill('SIGINT')
+	})
 	var check2 = cp.fork('./check2.js', [], [ 'pipe', 'pipe', 'pipe', 'ipc' ]);
 	check2.on('message', m => {
 		//Recebe mensagem para matar o processo filho.
