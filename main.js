9a413f47c8d601e96174e2ba33ec226c90d4c5c1
Gabriel Carneiro <gabriel.carneiro97@live.com>
2017-11-21 08:46:17 -0200

    Arrumei um bug no main

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const cp = require('child_process'),
 	interval = consts.interval; //Define o intervalo como 60 segundos.
 	
 var invoke = () => {
-	Inicializa o processo filho onde acontecerá o check da caixa direcionada.
+	// Inicializa o processo filho onde acontecerá o check da caixa direcionada.
 	var check = cp.fork('./check.js', [], [ 'pipe', 'pipe', 'pipe', 'ipc' ]);
 	check.on('message', m => {
 		//Recebe mensagem para matar o processo filho.
