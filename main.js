73117972aa6d0851329b0e7040865586d4550c23
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-02-09 16:11:36 -0200

    Mudei algumas coisas

--- main.js
+++ main.js
@@ -5,15 +5,19 @@ const interval = consts.interval // Define o intervalo como 60 segundos.
 
 var invoke = () => {
   // Inicializa o processo filho onde acontecerá o check da caixa direcionada.
-  var check = cp.fork('./check.js', [], [ 'pipe', 'pipe', 'pipe', 'ipc' ]);
+  var check = cp.fork('./check.js', [], [ 'pipe', 'pipe', 'pipe', 'ipc' ])
   check.on('message', m => {
     // Recebe mensagem para matar o processo filho.
-    if (m.type === 'killme') check.kill('SIGINT')
+    if (m.type === 'killme') {
+      check.kill('SIGINT')
+    }
   })
-  var check2 = cp.fork('./check2.js', [], [ 'pipe', 'pipe', 'pipe', 'ipc' ]);
+  var check2 = cp.fork('./check2.js', [], [ 'pipe', 'pipe', 'pipe', 'ipc' ])
   check2.on('message', m => {
     // Recebe mensagem para matar o processo filho.
-    if (m.type === 'killme') check2.kill('SIGINT')
+    if (m.type === 'killme') {
+      check2.kill('SIGINT')
+    }
   })
 }
 
